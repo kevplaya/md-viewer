2b28b48331edd68e5d17c687ad2c4ca967fc47c8
kevplaya <tothebs@gmail.com>
2026-03-15 21:55:38 +0900

    feat: react-grab 개발 도구 적용
    - 개발 모드에서만 react-grab 스크립트 자동 주입
    - Cmd+C / Ctrl+C로 UI 요소의 컴포넌트 정보 복사 가능
    - 프로덕션 빌드에는 포함되지 않음

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'react-grab',
+      transformIndexHtml(html, ctx) {
+        if (ctx.server) {
+          return html.replace(
+            '</head>',
+            '    <script src="//unpkg.com/react-grab/dist/index.global.js" crossorigin="anonymous"></script>\n  </head>'
+          )
+        }
+        return html
+      },
+    },
+  ],
 })
